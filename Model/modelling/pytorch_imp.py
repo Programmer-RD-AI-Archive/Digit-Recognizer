e79474ef5e01502cd756576185da2788c328c460
Programmer-RD-AI <go2ranuga@gmail.com>
2022-04-29 17:42:58 +0530

    Update pytorch_imp.py
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

--- Model/modelling/pytorch_imp.py
+++ Model/modelling/pytorch_imp.py
@@ -34,12 +34,12 @@ class CNN(Module):
         self.conv3 = Conv2d(16, 32, (3, 3))
         self.conv4batchnorm = BatchNorm2d(32)
         self.conv5 = Conv2d(32, 64, (1, 1))
-        self.linear1 = Linear(64 * 1 * 1, 128)
-        self.linear2batchnorm = BatchNorm1d(128)
-        self.linear3 = Linear(128, 256)
-        self.linear4batchnorm = Linear(256, 512)
-        self.linear5 = Linear(512, 256)
-        self.output = Linear(256, idx_of_classes)
+        self.linear1 = Linear(64 * 1 * 1, 256)
+        self.linear2batchnorm = BatchNorm1d(256)
+        self.linear3 = Linear(256, 512)
+        self.linear4batchnorm = Linear(512, 1024)
+        self.linear5 = Linear(1024, 512)
+        self.output = Linear(512, idx_of_classes)
 
     def forward(self, X):
         """sumary_line"""
