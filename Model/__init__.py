339c56cdbb3cc96631bd4a204033a7a519efac1f
Programmer-RD-AI <go2ranuga@gmail.com>
2022-04-29 12:28:33 +0530

    Update __init__.py
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

--- Model/__init__.py
+++ Model/__init__.py
@@ -7,17 +7,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import torch
-from dataset.dataset import *
-from dataset.preprocessing import *
+
 from PIL import Image
 from sklearn.model_selection import train_test_split
 from torch.nn import *
 from torch.optim import *
 from torchvision.transforms import *
 from tqdm import tqdm
+import wandb
+from torchvision.models import *
 
 SAVE_DIR = "./dataset/data/"
 RAW_DIR = "./dataset/raw/"
 TEST_SIZE = 0.25
 EPOCHS = 100
 BATCH_SIZE = 32
+DEVICE = "cuda"
+PROJECT_NAME = "Digit-Recognizer-Competition"
+
+from Model.dataset.preproccessing import *
+from Model.dataset.dataset import *
+from Model.help_funcs import *
+from Model.modelling.pytorch_imp import *
