fc97d35c6eb9b3d9a54fdbe4a4f5e443a4e72131
Programmer-RD-AI <go2ranuga@gmail.com>
2022-04-29 12:09:25 +0530

    History
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ .history/Model/metrics_20220429120652.py
@@ -0,0 +1,10 @@
+"""sumary_line"""
+from Model import *
+
+
+class Metrics:
+    def loss(self):
+        pass
+
+    def accuracy(self):
+        pass
---
+++ .history/Model/metrics_20220429120826.py
@@ -0,0 +1,19 @@
+"""sumary_line"""
+from Model import *
+
+
+class Metrics:
+    def loss(self, model, X, y, criterion):
+        preds = model(X)
+        loss = criterion(preds, y)
+        return loss.item()
+
+    def accuracy(self, model, X, y):
+        preds = model(X)
+        for pred, y_iter in tqdm(zip(preds, y)):
+            pred = torch.argmax(pred)
+            y_iter = torch.argmax(y_iter)
+            if pred == y_iter:
+                correct += 1
+            total += 1
+        return round(correct / total, 3)
