94ff7eee002da3b358ac59d44908ce1874509528
Programmer-RD-AI <go2ranuga@gmail.com>
2022-06-15 08:24:53 +0530

    Update pytorch.py
    Co-Authored-By: Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>

--- Model/modelling/pytorch.py
+++ Model/modelling/pytorch.py
@@ -1,1 +1,15 @@
 from Model import *
+
+
+class Clf(Module):
+    def __init__(self, classes: list):
+        self.activation = ReLU()
+        self.linear1 = Linear(28 * 28, 128)
+        self.linear2 = Linear(128, 256)
+        self.batchnorm = BatchNorm1d(256)
+        self.linear3 = Linear(256, 512)
+        self.linear4 = Linear(512, 256)
+        self.output = Linear(256, len(classes))
+
+    def forward(self, X):
+        pass
